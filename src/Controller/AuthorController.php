507105749ca8937a9bc3a397968cd27aa726af50
NewennT <newenn.turbiau@gmail.com>
2025-01-06 22:06:26 +0100

    Leçon sur AuthorController

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Author;
+use App\Form\AuthorType;
+use Symfony\Component\HttpFoundation\Request;
+
+#[Route('/admin/author')]
+class AuthorController extends AbstractController
+{
+    #[Route('', name: 'app_admin_author_index')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_author_new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $author = new Author();
+        $form = $this->createForm(AuthorType::class, $author);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            return $this->redirectToRoute('app_admin_author_new');
+        }
+
+        return $this->render('admin/author/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
