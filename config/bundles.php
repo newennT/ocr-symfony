567aef52e45dac5cfa3e02a2f791b6da8db24402
NewennT <newenn.turbiau@gmail.com>
2025-01-08 22:26:11 +0100

    User, Registration, Login

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
