33cb663a3b6472f465c2258119e4dc84c2924b8b
NewennT <newenn.turbiau@gmail.com>
2025-01-08 20:53:28 +0100

    Affichage des formulaires d'ajout, mise en place des controllers

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -7,24 +7,34 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Book;
 use App\Form\BookType;
+use App\Repository\BookRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 
 #[Route('/admin/book')]
 class BookController extends AbstractController
 {
     #[Route('', name: 'app_admin_book_index')]
-    public function index(): Response
+    public function index(Request $request, BookRepository $repository): Response
     {
+        $books = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            new QueryAdapter($repository->createQueryBuilder('b')),
+            $request->query->get('page', 1),
+            16
+        );
+
         return $this->render('book/index.html.twig', [
-            'controller_name' => 'AuthorController',
+            'books' => $books,
         ]);
     }
 
     #[Route('/new', name: 'app_admin_book_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/{id}/edit', name: 'app_admin_author_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function new(?Book $book, Request $request, EntityManagerInterface $entityManager): Response
     {
-        $book = new Book();
+        $book ??= new Book();
         $form = $this->createForm(BookType::class, $book);
 
         $form->handleRequest($request);
@@ -32,11 +42,19 @@ class BookController extends AbstractController
         {
             $entityManager->persist($book);
             $entityManager->flush();
-            return $this->redirectToRoute('app_admin_book_new');
+            return $this->redirectToRoute('app_admin_book_show', ['id' => $book->getId()]);
         }
 
         return $this->render('admin/book/new.html.twig', [
             'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_book_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(?Book $book): Response 
+    {
+        return $this->render('book/show.html.twig', [
+            'book' => $book,
         ]);
     }
 }
--- src/Repository/AuthorRepository.php
+++ src/Repository/AuthorRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Author;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -14,6 +15,24 @@ class AuthorRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Author::class);
+    }
+
+    public function findByDateOfBirth(array $dates = []): QueryBuilder 
+    {
+        $qb = $this->createQueryBuilder('a');
+
+        if(\array_key_exists('start', $dates)) {
+            $qb->andWhere('a.dateOfBirth >= :start')
+                ->setParameter('start', new \DateTimeImmutable($dates['start']));
+        }
+
+        if(\array_key_exists('end', $dates)) {
+            $qb->andWhere('a.dateOfBirth >= :end')
+                ->setParameter('end', new \DateTimeImmutable($dates['end']));
+        }
+
+        return $qb;
+        
     }
 
     //    /**
